32c11333f8855bcedeb20bfe4546a73d3865cf29
Arch <chqteng1234@163.com>
2025-03-04 20:57:32 +0800

    Mar 4 21:00PM
    The end of the Day !

---
+++ include/TreeNode_CPP.h
@@ -0,0 +1,22 @@
+#ifndef TREENODE_CPP_H
+#define TREENODE_CPP_H
+
+#include <iostream>
+
+class TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val) : val(val) , left(nullptr) , right(nullptr) {};
+
+    ~TreeNode() { delete left ; delete right; };
+
+};
+
+TreeNode* createTreeNode(int val){
+    return new TreeNode(val);
+}
+
+
+#endif
