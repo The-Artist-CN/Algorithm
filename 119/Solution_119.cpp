bc5ba929d2c419a8726b93d59cb658634fb51b13
Arch <chqteng1234@163.com>
2025-04-03 16:33:07 +0800

    Apr 3 15:00PM
    The start of test preparation

--- 119/Solution_119.cpp
+++ 119/Solution_119.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Solution {
+class First {
 public:
     vector<int> getRow(int number){
         vector<int> dp( number + 1 , 1 );
@@ -18,11 +18,30 @@ public:
 };
 
 
+//Apr 3 7:00AM
+
+class Second {
+public:
+    vector<int> getRow(int num){
+        vector<vector<int>> result(num + 1);
+        for (int i = 0;i <= num ; i++){
+            result[i].resize(i+1);
+            result[i][0] = result[i][i] = 1;
+            for (int j = 1 ; j < i ; j++){
+                result[i][j] = result[i-1][j-1] + result[i-1][j];
+            }
+        }
+
+        return result[num];
+    }
+};
+
+
 int main(){
-    Solution solution;
+    Second second;
     int n;
     cin >> n;
-    vector<int> result = solution.getRow(n);
+    vector<int> result = second.getRow(n);
     for ( int r : result ){
         cout << r << " ";
     }
