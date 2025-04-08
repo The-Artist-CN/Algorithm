89d1e088b5155d3f726079fb80f957c1bf6bd799
Arch <chqteng1234@163.com>
2025-04-08 20:19:13 +0800

    Apr 8 20:00PM

---
+++ 563/Solution_563.cpp
@@ -0,0 +1,76 @@
+// 
+//输入：root = [1,2,3]
+//输出：1
+//解释：
+//节点 2 的坡度：|0-0| = 0（没有子节点）
+//节点 3 的坡度：|0-0| = 0（没有子节点）
+//节点 1 的坡度：|2-3| = 1（左子树就是左子节点，所以和是 2 ；右子树就是右子节点，所以和是 3 ）
+//坡度总和：0 + 0 + 1 = 1
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [4,2,9,3,5,null,7]
+//输出：15
+//解释：
+//节点 3 的坡度：|0-0| = 0（没有子节点）
+//节点 5 的坡度：|0-0| = 0（没有子节点）
+//节点 7 的坡度：|0-0| = 0（没有子节点）
+//节点 2 的坡度：|3-5| = 2（左子树就是左子节点，所以和是 3 ；右子树就是右子节点，所以和是 5 ）
+//节点 9 的坡度：|0-7| = 7（没有左子树，所以和是 0 ；右子树正好是右子节点，所以和是 7 ）
+//节点 4 的坡度：|(3+5+2)-(9+7)| = |10-16| = 6（左子树值为 3、5 和 2 ，和是 10 ；右子树值为 9 和 7 ，和是 1
+//6 ）
+//坡度总和：0 + 0 + 0 + 2 + 7 + 6 = 15
+// 
+//
+// 示例 3： 
+// 
+// 
+//输入：root = [21,7,14,1,1,2,2,3,3]
+//输出：9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目的范围在 [0, 10⁴] 内 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 326 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+
+    int findTilt(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+
+    int dfs(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        int sumLeft = dfs(node->left);
+        int sumRight = dfs(node->right);
+        ans += abs(sumLeft - sumRight);
+        return sumLeft + sumRight + node->val;
+    }
+};
