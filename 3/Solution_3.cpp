19a343c8f796696fafbaf6ee5d0585259d12b9d0
Arch <chqteng1234@163.com>
2025-04-22 20:28:42 +0800

    Apr 22 21:00PM

---
+++ 3/Solution_3.cpp
@@ -0,0 +1,25 @@
+#include <unordered_set>
+#include <algorithm>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int left = 0;
+        int maxLength = 0;
+        
+        for (int right = 0; right < s.size(); right++) {
+            // If the character is already in the set, remove characters from the left
+            while (charSet.find(s[right]) != charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
+            }
+            // Add the current character to the set
+            charSet.insert(s[right]);
+            // Update the maximum length
+            maxLength = max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+};
