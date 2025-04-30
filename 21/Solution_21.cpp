6f5a041030666557a58697c9b72f872b306931c9
Arch <chqteng1234@163.com>
2025-04-30 21:55:47 +0800

    Update Solution_21.cpp

--- 21/Solution_21.cpp
+++ 21/Solution_21.cpp
@@ -12,7 +12,6 @@ public:
                 list2 = list2->next;
             }
         }
-        // 处理剩余部分
         while (list1 != nullptr) {
             values.push_back(list1->val);
             list1 = list1->next;
@@ -21,7 +20,7 @@ public:
             values.push_back(list2->val);
             list2 = list2->next;
         }
-        // 构建新链表
+
         ListNode* dummy = new ListNode(0);
         ListNode* curr = dummy;
         for (int val : values) {
@@ -45,7 +44,6 @@ public:
             prev = prev->next;
         }
 
-        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
         prev->next = l1 == nullptr ? l2 : l1;
 
         return preHead->next;
